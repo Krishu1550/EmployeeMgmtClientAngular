4395b0193870a52f2ca20c7c1a9cd882e37ed264
Krishu1550 <vtu10260@veltechuniv.edu.in>
2023-05-10 17:00:42 +0200

    InterFace

---
+++ src/app/interfaces/student.ts
@@ -0,0 +1,9 @@
+import { StudentCourse } from './studentCourse';
+
+export interface Student {
+  studentId: number;
+  studentName: string;
+  email: string;
+  dateOfBirth?: Date;
+  studentCourses?: StudentCourse[];
+}

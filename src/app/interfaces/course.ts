49f9b9bd9587d069428d8e4592481f250e6f456a
RATHWATHTHAGE Randika Wishwani <randika.wishwani@gmail.com>
2023-05-10 12:32:59 +0200

    add interfaces

---
+++ src/app/interfaces/course.ts
@@ -0,0 +1,13 @@
+import { StudentCourse } from "./studentCourse";
+import { Teacher } from "./teacher";
+
+export interface Course {
+    courseId: number;
+    courseName: string;
+    numOfSlot: number;
+    numOfClassPerWeek: number;
+    teacherId: number;
+    teacher?: Teacher;
+    assignmentFile?: string;
+    studentCourses?: StudentCourse[];
+  }
---
+++ src/app/interfaces/studentCourse.ts
@@ -0,0 +1,10 @@
+import { Course } from "./course";
+import { Student } from "./student";
+
+export interface StudentCourse {
+    studentCourseId: number;
+    studentId: number;
+    student?: Student;
+    courseId: number;
+    course?: Course;
+  }
